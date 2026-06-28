f91edf41a7c4247b3a994815aad669e7c966717c
Parvesh Kumar Sharma <parveshsharma8910@gmail.com>
2026-06-28 13:33:43 +0530

    implement google gemini-2.5-flash for summary module

--- summary_module.py
+++ summary_module.py
@@ -1,23 +1,44 @@
-"""
-Summary Module - Educational Text Summarization
+import os
+import google.generativeai as genai
 
-Summarizes long educational texts into concise, key-point summaries.
-Endpoint: POST /summarize
+# extracting the api key from the .env file
+api_key = os.getenv("GOOGLE_API_KEY")
 
-TODO: Implement the AI logic for text summarization.
-"""
+# configuring the genai
+if api_key:
+    genai.configure(api_key=api_key)
+else:
+    print("Gemini Api not configured. Environment Variable not found")
 
-
+# main function
 def get_summary(text: str) -> str:
-    """
-    Takes a block of educational text and returns a concise summary
-    highlighting the key points.
 
-    Args:
-        text (str): The educational text to summarize.
+    # if no text is provided - edge case
+    if not text.strip():
+        return "You need to provide a string to get the summary"
 
-    Returns:
-        str: A concise summary of the provided text.
-    """
-    # TODO: Integrate Google Generative AI or local model here
-    return f"[Summary Module] Summary of provided text — Not yet implemented."
+
+    try:
+        # initializing the model - used gemini 2.5 flash model
+        model = genai.GenerativeModel("gemini-2.5-flash") 
+
+        # prompt for summary generation
+        prompt = f"""
+            You are an expert educational assistant. Summarize the following long paragraphs into a concise, easy-to-understand version, making it ideal for quick revision. Ensure the core information is retained while eliminating redundancy. Format the output using clear, clean bullet points where appropriate.
+
+            Text to summarize:{text}
+        """
+
+        # obtaining the response - temp = 0.5
+        response = model.generate_content(prompt,
+            generation_config=genai.types.GenerationConfig(
+                temperature=0.5
+            )
+        )
+
+        # returning the generated summary
+        return response.text.strip()
+
+    # error handling
+    except Exception as e:
+        return f"Error while generating the summary:\n{str(e)}"
